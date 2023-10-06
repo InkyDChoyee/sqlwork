2e6fadc1157d2fcbdaecc4ba9e85709a5115386d
InkyDChoyee <choyee0421@naver.com>
2023-10-06 15:18:29 +0900

    sql source

--- systemdb/ex4.sql
+++ systemdb/ex4.sql
@@ -6,7 +6,11 @@ CREATE TABLE ex4(
 
 INSERT INTO ex4 VALUES ('sky2003', 'u1234');
 INSERT INTO ex4 VALUES ('sky2003', 'u54321');   -- 무결성 제약조건 위배
+INSERT INTO ex4 VALUES ('todat123', 'u23456', 25);
 
 COMMIT;
+
+-- 나이(age) 칼럼 추가
+ALTER TABLE ex4 ADD age NUMBER(3);
 
 SELECT * FROM ex4;
