fd353fb4d1a36ebb7cc096189feaf3b69b22a997
InkyDChoyee <choyee0421@naver.com>
2023-11-29 13:50:25 +0900

    new.member_table

---
+++ oracleworks/jwebdb/member.sql
@@ -0,0 +1,16 @@
+-- jweb, member 테이블 생성
+CREATE TABLE member(
+    mno       INT,
+    id           VARCHAR2(20) NOT NULL UNIQUE,
+    passwd    VARCHAR2(20) NOT NULL,
+    name       VARCHAR2(30) NOT NULL,
+    email       VARCHAR2(50),
+    gender     VARCHAR2(6),
+    joindate    TIMESTAMP DEFAULT SYSTIMESTAMP,
+    PRIMARY KEY(mno)
+);
+
+CREATE SEQUENCE seq_mno NOCACHE;
+
+INSERT INTO member(mno, id, passwd, name, email, gender)
+VALUES(seq_mno, 'khit', 'm123467', '김기용', 'khit@gamil.com', '남');
